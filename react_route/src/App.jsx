55310fe37bbbea5c2b9b9ce790a2aa44c4823660
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-11-18 20:18:58 +0800

    react_route

---
+++ react_route/src/App.jsx
@@ -0,0 +1,37 @@
+// 創建外殼組件 App
+import {Component} from 'react'
+import {Route} from 'react-router-dom'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink'
+import Item1 from './pages/Item1'
+import Item2 from './pages/Item2'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Header />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            <MyNavLink to='/item1'>Item 1</MyNavLink>
+                            <MyNavLink to='/item2'>Item 2</MyNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                <Route path='/item1' component={Item1}></Route>
+                                <Route path='/item2' component={Item2}></Route>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ react_route/src/pages/Item1/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Item1 extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Item1 內容</h2>
+            </div>
+        );
+    }
+}
